8f25d16748bf212d7496f3e89cc17e1d8a2ef5ab
Gabriel Nog <biel.snogueira@hotmail.com>
2024-09-23 19:03:44 -0300

    Estrutura inicial de UI

---
+++ culture-verse/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Homeaa />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Home from "./components/Home.vue";
+
+@Options({
+  components: {
+    Home,
+  },
+})
+export default class App extends Vue {}
+</script>
---
+++ culture-verse/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hello">hello</div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Homeaa extends Vue {}
+</script>
